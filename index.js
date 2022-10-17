e6e7e85f27be0d89b9cd6703edfe405c408342a3
Md Anisuzzaman <anisictiu@gmail.com>
2022-10-17 18:14:48 +0600

    works update

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('Heelo world')
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('./swagger.json');
+let swagger = JSON.parse(rawdata);
+
+
+//console.log(swagger.components.schemas);
+
+if (swagger && swagger.components && swagger.components.schemas) {
+
+    /*              if(schema && schema.properties)
+                 {
+ 
+                 } */
+    let obj = swagger.components.schemas;
+    for (const key of Object.keys(obj)) {
+        let modelName = key;
+        console.log(modelName);
+
+        const model = obj[key];
+        for (const key of Object.keys(model.properties)) {
+            let modelsPropertyName =key;
+            let modelsPropertyType =model.properties[key].type;
+            console.log(modelsPropertyName,modelsPropertyType);
+        }
+        
+        
+    }
+}
+
